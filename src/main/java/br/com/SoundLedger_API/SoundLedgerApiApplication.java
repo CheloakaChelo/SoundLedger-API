f1b746e0a8c27e56c258a32db21cab693ffb4c5a
CeloJay <jmgamer.360@gmail.com>
2024-11-11 12:29:32 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/SoundLedger_API/SoundLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.SoundLedger_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundLedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundLedgerApiApplication.class, args);
+	}
+
+}
