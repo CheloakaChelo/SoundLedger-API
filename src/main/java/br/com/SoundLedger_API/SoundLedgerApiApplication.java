ad86a82cf40b0d724b311949a2b2d71e066f3b13
chelo <jmjoao8@hotmail.com>
2025-07-27 12:24:38 -0300

    exclude SecurityConfiguration.class

--- src/main/java/br/com/SoundLedger_API/SoundLedgerApiApplication.java
+++ src/main/java/br/com/SoundLedger_API/SoundLedgerApiApplication.java
@@ -2,8 +2,9 @@ package br.com.SoundLedger_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class SoundLedgerApiApplication {
 
 	public static void main(String[] args) {
