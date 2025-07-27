9cf7a4479d78dd2a5548d0a25814d3b3329c51ba
chelo <jmjoao8@hotmail.com>
2025-07-27 12:24:12 -0300

    Participaçõa na Musica

--- src/main/java/br/com/SoundLedger_API/model/entity/Musica.java
+++ src/main/java/br/com/SoundLedger_API/model/entity/Musica.java
@@ -1,24 +1,28 @@
 package br.com.SoundLedger_API.model.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Document(collection = "musica_entity")
+@Data
 public class Musica {
 
     @Id
-    private Long id;
+    private String id;
 
     private String titulo;
-    private String artistaPrincipal;
+    private String artistaPrincipalId;
     private String album;
     private String isrc;
     private Integer duracaoSegundos;
 
     private String idSpotify;
     private String contratoBlockchain;
+
+    private List<ParticipacaoNaMusica> participacoes;
 
 
 }
