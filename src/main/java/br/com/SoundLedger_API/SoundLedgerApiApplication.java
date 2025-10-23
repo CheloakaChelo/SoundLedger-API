22990e1808bcb15c57a8dd86749687c854dbfd2d
chelo <jmjoao8@hotmail.com>
2025-10-23 12:04:05 -0300

    EnableScheduling

--- src/main/java/br/com/SoundLedger_API/SoundLedgerApiApplication.java
+++ src/main/java/br/com/SoundLedger_API/SoundLedgerApiApplication.java
@@ -3,8 +3,10 @@ package br.com.SoundLedger_API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@EnableScheduling
 public class SoundLedgerApiApplication {
 
 	public static void main(String[] args) {
